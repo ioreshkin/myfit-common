5b61d85694989fa6aae986b697eb8fcfc5a8db34
sergey-oreshkin <sergey.oreshkin@x5.ru>
2024-11-30 22:00:10 +0300

    base starter

---
+++ src/main/java/center/myfit/starter/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package center.myfit.starter.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(
+      HttpSecurity http, @Value("${cors.url}") String corsUrl) throws Exception {
+
+    http.csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
+    if (StringUtils.hasText(corsUrl)) {
+      http.cors(cors -> cors.configurationSource(corsConfigurationSource(corsUrl)));
+    }
+    return http.build();
+  }
+
+  private UrlBasedCorsConfigurationSource corsConfigurationSource(String corsUrl) {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList(corsUrl));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
