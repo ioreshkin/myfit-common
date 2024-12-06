5559838134b3d37425c86793bebe75b3b9db6c67
PavelSharko <pushkin.94.72@gmail.com>
2024-12-06 18:45:43 +0700

    переделал через @Pattern Url

--- src/main/java/center/myfit/starter/dto/CreateExerciseDto.java
+++ src/main/java/center/myfit/starter/dto/CreateExerciseDto.java
@@ -1,30 +1,30 @@
 package center.myfit.starter.dto;
 
-import center.myfit.starter.dto.customvalidation.UrlOrEmpty;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 /**
  * Data Transfer Object for creating an exercise.
  */
 public record CreateExerciseDto(
-        Long id,
+    Long id,
 
-        @NotBlank(message = "Title cannot be empty")
-        @Size(min = 3, max = 64, message = "Title must be between 3 and 64 characters")
-        String title,
+    @NotBlank(message = "Title cannot be empty")
+    @Size(min = 3, max = 64, message = "Title must be between 3 and 64 characters")
+    String title,
 
-        @NotBlank(message = "Description cannot be empty")
-        String description,
+    @NotBlank(message = "Description cannot be empty")
+    String description,
 
-        @UrlOrEmpty
-        String videoUrl,
+    @Pattern(regexp = "^(http|https)://.*|^$", message = "Must be a valid URL or empty")
+    String videoUrl, // Используем @Pattern для валидации URL
 
-        ImageDto image
+    ImageDto image
 ) {
   /**
-  * Data Transfer Object for image associated with an exercise.
-  */
+   * Data Transfer Object for image associated with an exercise.
+   */
   public record ImageDto(
       String original,
       String mobile,
